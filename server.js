353aa0b9454ad0d4459689705447d9861c88824b
cerebrium <nicholas.m.shankland@gmail.com>
2019-10-08 11:21:55 -0700

    server running

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+// requiring
 const express = require('express');
 const app = express();
 const ejsLayouts = require('express-ejs-layouts');
-const fs = require('fs')
-
+const fs = require('fs');
+app.set('view engine', 'ejs');
+app.use(express.static('static'))
 app.use(ejsLayouts)
 
 
-app.listen(3004, function() {
+
+app.get(`/`, function(req, res) {
+    res.send('root route')
+})
+
+
+app.listen(3005, function() {
     console.log('server is running')
 })
