be3c5e74ce77b7eab380602b14292dbc1f851cc2
cerebrium <nicholas.m.shankland@gmail.com>
2019-10-08 15:44:07 -0700

    did the code along lab thing there

--- routes/cryptids.js
+++ routes/cryptids.js
@@ -1,1 +1,32 @@
 // will hold all routes for the cryptids
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+router.get(`/`, function (req, res) {
+    let cryptidsJson = fs.readFileSync(`./cryptids.json`)
+    let cryptidsData = JSON.parse(cryptidsJson)
+    res.render('cryptids/index', {cryptid: cryptidsData})
+});
+
+router.post(`/`, function(req, res) {
+    let cryptidsJson = fs.readFileSync(`./cryptids.json`)
+    let cryptidsData = JSON.parse(cryptidsJson);
+    cryptidsData.push(req.body)
+    fs.writeFileSync(`./cryptids.json`, JSON.stringify(cryptidsData))
+    res.redirect(`cryptids/`)
+})
+
+router.get(`/new`, function(req, res) {
+    res.render(`cryptids/new`)
+})
+
+router.get(`/:id`, function(req, res) {
+    let cryptidID = parseInt(req.params.id);
+    let cryptidsJson = fs.readFileSync(`./cryptids.json`);
+    let cryptidsData = JSON.parse(cryptidsJson);
+    res.render(`cryptids/show`, {crypt: cryptidsData[cryptidID]})
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get(`/`, function(req, res) {
     res.render(`home`)
 })
 
+app.use(`/cryptids`, require(`./routes/cryptids`))
 app.use(`/dinosaurs`, require(`./routes/dinosaurs`))
 
 app.listen(3005, function() {
